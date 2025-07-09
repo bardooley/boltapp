703a1343119113e84e4b51a4ffd6e1bed5e9f961
bardooley <124945376+bardooley@users.noreply.github.com>
2025-07-09 00:29:33 -0400

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -50,6 +50,8 @@ const Footer = () => {
           
 
           
+        </div>
+
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
           <p>&copy; 2024 Wild Horse. All rights reserved.</p>
         </div>
